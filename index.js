27ed6c615fe72011aa602c0feee7fd0ec77e28a2
Allan BUSI <busi.travail@gmail.com>
2023-06-05 16:43:00 +0200

    main

--- index.js
+++ index.js
@@ -9,17 +9,7 @@ app.use(bodyParser.urlencoded({extended: false}))
 // première routes en get 
 app.get('/', function (req, res) {
 
-    let number1 = Number(req.query.number1)
-    let number2 = Number(req.query.number2)
 
-    // faire une conditions qui permet de savoir si c'est un chiffre ou pas ? 
-    if (isNaN(number1) || isNaN(number2)) {
-        
-        return res.status(409).json({
-            error: true,
-            message: ["Vous n'avez pas envoyé de nombre"],
-        })
-    }
 
     return res.status(200).json({
         error: false,
@@ -28,20 +18,122 @@ app.get('/', function (req, res) {
     })
 })
 
-app.post('/', function (req, res) {
 
-    // nom, prenom, date_naissance, email, phone
-    // nom = 1-155, prenom = 1,155, date_naissance = +18 ok sinon erreur, email = type email, phone = 8, 12 
-    
-    
-
-    return res.status(200).json({
-        error: false,
-        message: ["Hello world"]
+app.put('/', function (req,res) {
+    return res.status(409).json({
+        error: true,
+        message: ["Element modifier"]
     })
 })
 
+app.delete('/', function (req,res) {
+    return res.status(409).json({
+        error: true,
+        message: ["Element supprimer"]
+    }) 
+})
+
+app.post('/middleware',
+(req, res, next) => {
+    let body = req.body
+
+    let error = []
+
+    if (!body.nom) {
+        error.push("Le nom est incorrecte")
+    }
+
+    if (!body.prenom) {
+        error.push("Le prenom est incorrecte")
+    }
+
+    if (!body.email) {
+        error.push("Le email est incorrecte")
+    }
+
+    if (!body.phone) {
+        error.push("Le phone est incorrecte")
+    }
+
+    if (!body.date) {
+        error.push("Le date est incorrecte")
+    }
+
+
+    if (error.length > 0) {
+        return res.status(409).json({
+            error: true,
+            message: error
+        })
+    }
+
+    if (body.nom.length <= 1 || body.nom.length >= 155) {
+        error.push("Le nom est incorrecte")
+    }
+
+    if (body.prenom.length <= 1 || body.prenom.length >= 155) {
+        error.push("Le prenom est incorrecte")
+    }
+
+    if (body.phone.length <= 8 || body.phone.length >= 12) {
+        error.push("Le phone est incorrecte")
+    }
+
+    if (!String(body.email).toLowerCase().match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    )) {
+        error.push("Le email est incorrecte")
+    }
+
+     // Récupérer la date actuelle
+    var currentDate = new Date();
+    
+    // Séparer la date en année, mois et jour
+    var parts = body.date.split("-");
+    var year = parseInt(parts[0], 10);
+    var month = parseInt(parts[1], 10);
+    var day = parseInt(parts[2], 10);
+    
+    // Créer une nouvelle date à partir de la date fournie
+    var userDate = new Date(year, month - 1, day);
+    
+    // Calculer la différence en millisecondes entre les deux dates
+    var diff = currentDate - userDate;
+    
+    // Calculer l'âge en années
+    var age = Math.floor(diff / (1000 * 60 * 60 * 24 * 365.25));
+
+    
+    // Vérifier si l'utilisateur a plus de 18 ans
+    if (age <= 18) {
+        error.push("Vous n'êtes pas majeur car vous avez "+age+" ans");
+    } 
+
+    if (error.length > 0) {
+        return res.status(409).json({
+            error: true,
+            message: error
+        })
+    }
+    
+    return next()
+},
+(req, res) => {
+    let body = req.body
+
+    return res.status(200).json({
+        error: false,
+        message: ["Hello world"],
+    })
+}
+)
+
+
+
+
+
+
 // declanche le serveur sur le port 3000
-app.listen(3000, () => {
-    console.log("Server start localhost:3000")
+app.listen(4000, () => {
+    console.log("Server start localhost:4000")
 })
